b284d427cb4b72c957fc3e301f0b38fc353a40d0
Tristan Rice <rice@outerearth.net>
2013-06-16 00:58:58 -0400

    Slide scaling & keyboard nav

--- assets/javascripts/presentation.js
+++ assets/javascripts/presentation.js
@@ -1,10 +1,10 @@
 // WebSync: Page layout handler
 define("/assets/presentation.js",['websync'], function(websync) { var self = this;
-    $(".content").hide().addClass("content-presentation").fadeIn();
+    $(".content").hide().fadeIn();
+    $("body").addClass("layout-presentation");
     $(".content").append($('<div id="slides" class="content_container"></div>'));
     $('body').append($('<div id="presentation-nav" class="sidebar"><button id="addSlide" class="btn" type="button">Add Slide</button> <button id="remSlide" class="btn btn-danger" type="button">Delete Slide</button><div id="slideView" class="slideWell"></div></div>'));
-    $('#presentation-nav').css({left: -252}).animate({left:-1},200);
-    $('.content_well').animate({left: 251},200)
+    $('#presentation-nav').css({left: -1});
     $("#addSlide").click(function(){
         $(".slide.active").removeClass('active');
         $("<div class='slide active'></div>").appendTo($('#slides'));
@@ -48,6 +48,45 @@ define("/assets/presentation.js",['websync'], function(websync) { var self = thi
         });
     };
 	$(".content_well").children().bind("mousedown selectstart",function(e){ e.stopPropagation(); });
+    $(document).keydown(function(e){
+        if(WebSyncAuth.view_op=="view"){
+            if(e.keyCode==39||e.keyCode==32||e.keyCode==40){
+                // Move forward a slide
+                var cur_slide = $(".slidePreview.active").data().index;
+                $(".slide.active").removeClass('active');
+                $(".slidePreview.active").removeClass('active');
+                var next_slide = cur_slide+1;
+                var slide_num = $(".slide").length;
+                if(next_slide>=slide_num){
+                    next_slide = slide_num -1;
+                }
+                $($(".slide").get(next_slide)).addClass("active");
+                $($(".slidePreview").get(next_slide)).addClass("active");
+                e.preventDefault();
+            } else if(e.keyCode==37||e.keyCode==38){
+                // Move back a slide
+                var cur_slide = $(".slidePreview.active").data().index;
+                $(".slide.active").removeClass('active');
+                $(".slidePreview.active").removeClass('active');
+                var next_slide = cur_slide-1;
+                if(next_slide<=0){
+                    next_slide = 0;
+                }
+                $($(".slide").get(next_slide)).addClass("active");
+                $($(".slidePreview").get(next_slide)).addClass("active");
+                e.preventDefault();
+            }
+        }
+    });
+    $(window).bind("resize keydown",function(){
+        var well_rect = $(".content_well").get(0).getBoundingClientRect();
+        var content_rect = $(".slide.active");
+        var width_scale = well_rect.width/(content_rect.width()+60);
+        var height_scale = well_rect.height/(content_rect.height()+65);
+        var zoom = (width_scale>height_scale)*height_scale + (width_scale<=height_scale)*width_scale;
+        $("#slides").css({"transform":"scale("+zoom.toFixed(3)+")"});
+    });
     WebSync.fromJSON();
+    $(window).resize();
     return self;
 });

d61c899bbe211d0046ec7587fc465005fb6c3410
Tristan Rice <rice@outerearth.net>
2013-06-18 20:21:07 -0400

    Resizing handles added

--- assets/javascripts/resize.js
+++ assets/javascripts/resize.js
@@ -4,7 +4,29 @@ define(['websync'],function(){ var self = {};
     });
     self.resizeOn = function(elem){
         console.log(elem);
+        self.active = elem;
+        // Add handle DIVs
+        $(".content").append('<div class="Resize handle top left"></div>');
+        $(".content").append('<div class="Resize handle top middle"></div>');
+        $(".content").append('<div class="Resize handle top right"></div>');
+        $(".content").append('<div class="Resize handle right middle"></div>');
+        $(".content").append('<div class="Resize handle right bottom"></div>');
+        $(".content").append('<div class="Resize handle bottom middle"></div>');
+        $(".content").append('<div class="Resize handle bottom left"></div>');
+        $(".content").append('<div class="Resize handle left middle"></div>');
     };
+    self.updateHandles = function(){
+        var offset = $(self.elem).offset();
+        $(".Resize.handle.top").css({top: offset.top});
+        $(".Resize.handle.left").css({top: offset.left});
+        $(".Resize.handle.right").css({left: offset.left+$(self.elem).width()});
+        $(".Resize.handle.bottom").css({top: offset.top+$(self.elem).height()});
+        $(".Resize.handle.right.middle, .Resize.handle.left.middle").css({top: offset.top+$(self.elem).height()/2});
+        $(".Resize.handle.top.middle, .Resize.handle.right.middle").css({left: offset.left+$(self.elem).width()/2});
+    }
+    self. resizeOff = function(){
+        $(".Resize.handle").remove();
+    }
     self.disable = function(){
 		$("*").unbind(".Resize");
 		$("*").undelegate(".Resize");

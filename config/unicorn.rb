feeb1e85a312103328f1f306f93139325240f4d4
Tristan Rice <rice@outerearth.net>
2014-01-30 20:53:20 -0800

    Updates

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,6 +11,11 @@ timeout 30
 # we will use this in our nginx.conf later
 listen 4569, :backlog => 64
 
+# Create directories
+Dir.mkdir("tmp") unless Dir.exists? "tmp"
+Dir.mkdir("tmp/pids") unless Dir.exists? "tmp/pids"
+Dir.mkdir("log") unless Dir.exists? "log"
+
 # Set process id path
 pid "#{@dir}tmp/pids/unicorn.pid"
 

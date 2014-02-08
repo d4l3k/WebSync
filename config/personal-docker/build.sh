1c134a3ef1690411887355b2cc0718a9dddd2b89
Tristan Rice <rice@outerearth.net>
2014-02-07 23:22:32 -0800

    Updated docker scripts

--- config/personal-docker/build.sh
+++ config/personal-docker/build.sh
@@ -1,2 +1,6 @@
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 docker pull d4l3k/websync
 docker build -t websyncpersonal .
--- config/personal-docker/run.sh
+++ config/personal-docker/run.sh
@@ -1,1 +1,5 @@
-sudo docker run -i -p 4567:4567 -p 4568:4568 -u root -t websyncpersonal $*
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+docker run -i -p 4567:4567 -p 4568:4568 -u root -t websyncpersonal $*

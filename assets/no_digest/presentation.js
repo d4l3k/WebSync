6d9141842d27e1c83ae8f454de9400a144155435
Tristan Rice <rice@outerearth.net>
2014-02-13 19:45:48 -0800

    Fixed presentation.js

--- assets/no_digest/presentation.js
+++ assets/no_digest/presentation.js
@@ -86,7 +86,7 @@ define("/assets/presentation.js", ['websync'], function(websync) {
         var ctx = canvas.getContext("2d");
         var data = "<svg xmlns='http://www.w3.org/2000/svg' width='200' height='200'>" +
             "<foreignObject width='100%' height='100%'>" +
-            html "</foreignObject>" +
+            html + "</foreignObject>" +
             "</svg>";
         var DOMURL = window.URL || window.webkitURL || window;
         var img = new Image();

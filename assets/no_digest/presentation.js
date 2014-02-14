851e36a94df3206f86568d54b9b586f4c744d4d6
Tristan Rice <rice@outerearth.net>
2014-02-13 22:57:57 -0800

    A lot more test updates

--- assets/no_digest/presentation.js
+++ assets/no_digest/presentation.js
@@ -86,9 +86,9 @@ define("/assets/presentation.js", ['websync'], function(websync) {
         var ctx = canvas.getContext("2d");
         var data = "<svg xmlns='http://www.w3.org/2000/svg' width='200' height='200'>" +
             "<foreignObject width='100%' height='100%'>" +
-               "<div xmlns='http://www.w3.org/1999/xhtml'>" +
-                   html +
-               "</div>" +
+            "<div xmlns='http://www.w3.org/1999/xhtml'>" +
+            html +
+            "</div>" +
             "</foreignObject>" +
             "</svg>";
         var DOMURL = window.URL || window.webkitURL || window;
--- spec/selenium_spec.rb
+++ spec/selenium_spec.rb
@@ -25,12 +25,61 @@ describe "Complete Test" do
         sleep 1
         assert User.all(email: 'test@websyn.ca').documents.length == 1
         sleep 2
-        # Test title
+
+        # Test title change
         driver.find_element(:css, "#name").click
         driver.find_element(:css, "#name").send_keys "Toast"
         driver.find_element(:css, ".page").click
         sleep 2
         assert User.all(email: 'test@websyn.ca').documents[0].name == driver.find_element(:css, "#name").text, "Document title did not sync. Connection error?"
+
+        # Test body change
+        text = "I am quite fond of jelly bellies."
+        page = driver.find_element(:css, ".page")
+        page.click
+        page.send_keys text
+        sleep 1
+        doc = User.all(email: 'test@websyn.ca').documents[0]
+        assert doc.body["body"][0]["textContent"] == text, "Modify text"
+        assert doc.changes.length == 1, "Create patch"
+
+        # Test Permissions
+        driver2 = Selenium::WebDriver.for :firefox
+        driver2.navigate.to driver.execute_script("return window.location.href")
+        assert driver2.find_element(:css, ".panel-title").text=="Sign In"
+        driver.find_element(:id, "settingsBtn").click
+
+        # Test Hidden viewing mode
+        element = driver.find_element(:id, "access_mode")
+        select=Selenium::WebDriver::Support::Select.new(element)
+        select.select_by(:text, "Hidden (link only)")
+        driver2.navigate.to driver.execute_script("return window.location.href")
+
+        # This will throw an error if there is no button.
+        assert driver2.find_elements(:id, "settingsBtn").length==1
+        
+        # Test Public viewing mode.
+        select.select_by(:text, "Public")
+        driver2.navigate.to driver.execute_script("return window.location.href")
+        assert driver2.find_elements(:id, "settingsBtn").length==1
+        
+        # Close settings menu
+        driver.find_element(:id, "settingsBtn").click
+        
+        # Test Chat
+        driver.find_element(:id, "chat_btn").click
+        driver2.find_element(:id, "chat_btn").click
+        chat_input = driver.find_element(:id, "appendedInputButton")
+        chat_input.click
+        chat_input.send_keys "Hi there! How are you?"
+        driver.find_element(:id, "msg_btn").click
+        sleep 1
+        assert driver2.find_elements(:partial_link_text, 'test@websyn.ca').length==1
+        driver.find_element(:id, "chat_btn").click
+        driver2.find_element(:id, "chat_btn").click
+        
+        # Test Deletion
+        driver2.quit
         driver.find_element(:css, "#ribbon_buttons a").click
         driver.find_element(:css, "[href='delete']").click
         assert User.all(email: 'test@websyn.ca').documents.length == 0, "Document failed to be deleted."

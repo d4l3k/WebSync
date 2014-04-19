7bc6c8dd4fb47bbb02737354d9ae231ecb61f03f
Tristan Rice <rice@outerearth.net>
2014-04-18 23:39:47 -0700

    removed log file

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -8,25 +8,29 @@ end
 
 describe "WebSync Frontend" do
   it "should successfully load the home page" do
-    get '/' 
+    get '/'
     assert last_response.ok?
   end
   it "should successfully load the login page" do
-    get '/login' 
+    get '/login'
     assert last_response.ok?
   end
   it "should successfully redirect the upload page to login" do
-    get '/upload' 
+    get '/upload'
     assert last_response.redirect?
-    post '/upload' 
+    post '/upload'
     assert last_response.redirect?
   end
   it "should successfully redirect the admin page to login" do
-    get '/admin' 
+    get '/admin'
     assert last_response.redirect?
   end
+  it "should successfully load the public assets page" do
+    get '/public'
+    assert last_response.ok?
+  end
   it "should successfully load the documentation" do
-    get '/documentation' 
+    get '/documentation'
     assert last_response.ok?
   end
 end

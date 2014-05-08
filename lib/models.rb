32f3c6e4bec836a28e172f543e9a024bab6e73b6
Tristan Rice <rice@outerearth.net>
2014-05-07 19:01:19 -0700

    Fixed bug

--- lib/models.rb
+++ lib/models.rb
@@ -176,7 +176,7 @@ class User
     property :email, String, :key=>true
     property :password, BCryptHash
     property :group, String, :default=>'user'
-    property :create_time, DateTime, :default=> lambda{ DateTime.new }
+    property :create_time, DateTime, :default=> lambda{|a,b| DateTime.new }
     has n, :permissions
     has n, :files, model: WSFile, :through => :permissions
     has n, :changes

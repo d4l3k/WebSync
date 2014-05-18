955e6c85aacc48ce334eee52d3d6033a1fbf8fb1
Tristan Rice <rice@outerearth.net>
2014-05-17 20:04:46 -0700

    Only use 404 errors in production

--- lib/main.rb
+++ lib/main.rb
@@ -163,17 +163,19 @@ class WebSync < Sinatra::Base
         end
         redirect '/login'
     end
-    not_found do
-        erb :error, locals:{error: "404", reason: "Page or document not found."}
-    end
-    error 403 do
-        erb :error, locals:{error: "403", reason: "Access denied."}
-    end
-    error 400 do
-        erb :error, locals:{error: "400", reason: "Invalid request."}
-    end
-    error 500 do
-        erb :error, locals:{error: "500", reason: "The server failed to handle your request."}
+    configure :production, :development do
+        not_found do
+            erb :error, locals:{error: "404", reason: "Page or document not found."}
+        end
+        error 403 do
+            erb :error, locals:{error: "403", reason: "Access denied."}
+        end
+        error 400 do
+            erb :error, locals:{error: "400", reason: "Invalid request."}
+        end
+        error 500 do
+            erb :error, locals:{error: "500", reason: "The server failed to handle your request."}
+        end
     end
     get '/' do
         @javascripts = []

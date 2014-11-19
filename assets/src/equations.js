aed2c6f94d99a052239fead2502840024a6f8477
Tristan Rice <rice@fn.lc>
2014-11-18 16:57:50 -0800

    fixed #150

--- assets/src/equations.js
+++ assets/src/equations.js
@@ -1,12 +1,17 @@
+/*jslint browser: true*/
+/*global $, define, rangy, _, prompt, alert, Detector, WebSyncData, ace*/
+
 // WebSync LaTeX equation editing plugin.
-define(['websync'], function() {
+define(['websync'], function(WS) {
   var self = {};
   // Load the MathQuill javascript library.
   $('body').append('<script type="text/javascript" class="Equations" src="/assets/mathquill.js"></script><link rel="stylesheet" type="text/css" href="/mathquill.css">');
 
   // Append the new equation button to the File menu and make it do stuff.
   $('#Insert').append(" <button id='insert_equation' class='btn btn-default Equations' title='Insert Equation (Ctrl-Shift-F)'>Equation</button>");
-  $('#insert_equation').click(self.insertEquation);
+  $('#insert_equation').click(function() {
+    self.insertEquation();
+  });
   self.insertEquation = function() {
     var elem = $('<span class="Equations Equation-Editable" contenteditable="false"></span>')[0];
     rangy.getSelection().getAllRanges()[0].surroundContents(elem);
@@ -18,12 +23,12 @@ define(['websync'], function() {
     }, 1);
   };
   $(document).bind('keydown.Equations', function(e) {
-    if (e.keyCode == 70 && e.shiftKey && e.ctrlKey) {
+    if (e.keyCode === 70 && e.shiftKey && e.ctrlKey) {
       self.insertEquation();
     }
   });
   // Custom handler for equations since MathQuill doesn't export clean HTML.
-  WebSync.registerDOMException('.mathquill-rendered-math', function(obj) {
+  WS.registerDOMException('.mathquill-rendered-math', function(obj) {
     return $(obj).mathquill('latex');
   }, function(json) {
     setTimeout(function() {
@@ -36,7 +41,7 @@ define(['websync'], function() {
     $('*').unbind('.Equations');
     $('*').undelegate('.Equations');
     $('.Equations').remove();
-    WebSync.unregisterDOMException('.mathquill-rendered-math');
+    WS.unregisterDOMException('.mathquill-rendered-math');
 
   };
   // Return self so other modules can hook into this one.

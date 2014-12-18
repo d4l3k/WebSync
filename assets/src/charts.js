5c3923da5e68439032162a1fa2322ddd6983b831
Tristan Rice <rice@fn.lc>
2014-12-18 12:47:13 -0800

    Cleaned up charts.js and documented it

--- assets/src/charts.js
+++ assets/src/charts.js
@@ -1,42 +1,56 @@
 // WebSync: Charts for Spreadsheets
-/*global define, $, _, openpgp, escape, FileReader, JST*/
+/*global define, $, _, escape, FileReader, JST, document*/
 
 //= require templates/chart
 
 define(['websync', '/assets/tables.js', '/assets/Chart.js'], function(WS, tables, Chart) {
   'use strict';
-  var self = {};
+  var exports = {
 
-  // Button to insert chart. Might be moved under Table tab instead.
-  $('#Insert').append(" <button id='insert_chart' class='btn btn-default Chart' title='Insert Chart'><i class='fa fa-picture-o'></i></button>");
+  /**
+   * Create a palette of colors in hsla format. Opacities can be undefined or in the format of [1, 0.5, 0].
+   *
+   * @param {Number} number - the number of colors
+   * @param {Array<Number>} opacities - an optional array of opacities to generate colors with.
+   * @return {Array<String>}
+   */
+    colorWheel: function(number, opacities) {
+      var colors = [];
 
-  // Insert Modal
-  $(document.body).append(JST.get('templates/chart')());
-  $('#insertChartModal').find('input, select').change(function(e) {
-    updateModal();
-  });
-  var insertChart = false;
-  $('#insertChartModal #insertChartBtn').on('click.Chart', function(e) {
-    WS.info('Click to insert the chart.');
-    insertChart = true;
-  });
-  $('.content').delegate('td', 'mouseup.Chart', function(e) {
-    if (insertChart) {
-      $('#insertChartModal .Chart').appendTo(e.currentTarget);
-      insertChart = false;
-    }
-  });
-  $('.content_container').on('click.Chart', function(e) {
-    if (insertChart) {
-      var sel = WS.selectionSave();
-      $('#insertChartModal .Chart').appendTo(sel.endContainer);
-      insertChart = false;
-    }
-  });
-  // Create a chart in a container with options and size.
-  function makeChart(container, options, size) {
+      function makeHSLA(h, s, l, a) {
+        return 'hsla(' +
+          (h | 0) + ',' +
+          (s | 0) + '%,' +
+          (l | 0) + '%,' +
+          a + ')';
+      }
+
+      _.times(number, function(i) {
+        var h = i / number * 360;
+        var s = 100;
+        var l = 60;
+        var color;
+        if (opacities) {
+          color = _.map(opacities, function(opacity) {
+            return makeHSLA(h, s, l, opacity);
+          });
+        } else {
+          color = makeHSLA(h, s, l, 1);
+        }
+        colors.push(color);
+      });
+      return colors;
+    },
+
+    /**
+     * Create a chart in a container with options and size.
+     * @param {Element} container - the container to attach to.
+     * @param {Collection} options - a object with options
+     * @param {Collection} size - an object with width & height values.
+     */
+    makeChart: function(container, options, size) {
       var chart = $('<div class="Chart resizable" contenteditable="false">' +
-        (chartTitle ? '<h2>' + _.escape(options.chartTitle) + '</h2>' : '') +
+        (options.chartTitle ? '<h2>' + _.escape(options.chartTitle) + '</h2>' : '') +
         '<h3 class="y">' + _.escape(options.chartYTitle) + '</h3>' +
         '<canvas class="noresize"></canvas>' +
         '<h3>' + _.escape(options.chartXTitle) + '</h3>' +
@@ -71,15 +85,15 @@ define(['websync', '/assets/tables.js', '/assets/Chart.js'], function(WS, tables
         }, 100);
       }
 
-
-      chart.on('resize.Chart', function(e) {
+      chart.on('resize.Chart', function() {
         resize();
       });
 
       var cellData = tables.getCellData(options.dataRange);
+      var data, colors;
       if (_.include(['PolarArea', 'Pie', 'Doughnut'], options.type)) {
-        var data = [];
-        var colors = self.colorWheel(cellData[0].length, [1, 0.5]);
+        data = [];
+        colors = exports.colorWheel(cellData[0].length, [1, 0.5]);
         _.each(cellData[0], function(v, i) {
           data.push({
             label: v,
@@ -89,11 +103,11 @@ define(['websync', '/assets/tables.js', '/assets/Chart.js'], function(WS, tables
           });
         });
       } else {
-        var data = {
+        data = {
           labels: cellData[0].slice(options.firstRowAsTitles ? 1 : 0),
           datasets: []
         };
-        var colors = self.colorWheel(cellData.length);
+        colors = exports.colorWheel(cellData.length);
         _.each(cellData.slice(1), function(column, i) {
           var color = colors[i];
           var datum = {
@@ -130,9 +144,10 @@ define(['websync', '/assets/tables.js', '/assets/Chart.js'], function(WS, tables
       }
       resize();
       return [chart, chartObj];
-    }
+    },
+
     // Reset the chart insertion modal.
-  function clearModal() {
+    clearModal: function() {
       $('#chartType').val('Line');
 
       var table_count = $('.content_container table').index(tables.primaryTable()) + 1;
@@ -144,93 +159,96 @@ define(['websync', '/assets/tables.js', '/assets/Chart.js'], function(WS, tables
         tables.columnLabel(end[0]) + (end[1] + 1);
       $('#dataRange').val(queryString);
       tables.clearSelect();
-    }
+    },
+
     // Redraw the preview chart.
-  function updateModal() {
-    var chartType = $('#chartType').val().replace(/ /g, '');
-    $('.ChartContainer .Chart').remove();
-    $('#insertChartModal .options:not(.All)').hide();
-    $('#insertChartModal .options.' + chartType).show();
+    updateModal: function() {
+      var chartType = $('#chartType').val().replace(/ /g, '');
+      $('.ChartContainer .Chart').remove();
+      $('#insertChartModal .options:not(.All)').hide();
+      $('#insertChartModal .options.' + chartType).show();
 
-    var options = {
-      type: chartType
-    };
-    _.each($('#insertChartModal').find('.options.All, .options.' + chartType).find('input, select'), function(elem) {
-      var inputType = $(elem).attr('type');
-      if (inputType === 'checkbox') {
-        options[elem.id] = elem.checked;
-      } else {
-        options[elem.id] = $(elem).val();
-      }
-    });
+      var options = {
+        type: chartType
+      };
+      _.each($('#insertChartModal').find('.options.All, .options.' + chartType).find('input, select'), function(elem) {
+        var inputType = $(elem).attr('type');
+        if (inputType === 'checkbox') {
+          options[elem.id] = elem.checked;
+        } else {
+          options[elem.id] = $(elem).val();
+        }
+      });
 
-    makeChart($('.ChartContainer'), options, {
-      width: 558,
-      height: 300
-    });
-  }
+      exports.makeChart($('.ChartContainer'), options, {
+        width: 558,
+        height: 300
+      });
+    },
+
+    // Code to disable the function.
+    disable: function() {
+      $('*').unbind('.Chart');
+      $('*').undelegate('.Chart');
+      $('.Chart').remove();
+      WS.unregisterDOMException('.Chart');
+    }
+  };
+
+
+  // Button to insert chart. Might be moved under Table tab instead.
+  $('#Insert').append(" <button id='insert_chart' class='btn btn-default Chart' title='Insert Chart'><i class='fa fa-picture-o'></i></button>");
+
+  // Insert Modal
+  $(document.body).append(JST.get('templates/chart')());
+  $('#insertChartModal').find('input, select').change(function() {
+    exports.updateModal();
+  });
+  var insertChart = false;
+  $('#insertChartModal #insertChartBtn').on('click.Chart', function() {
+    WS.info('Click to insert the chart.');
+    insertChart = true;
+  });
+  $('.content').delegate('td', 'mouseup.Chart', function(e) {
+    if (insertChart) {
+      $('#insertChartModal .Chart').appendTo(e.currentTarget);
+      insertChart = false;
+    }
+  });
+  $('.content_container').on('click.Chart', function() {
+    if (insertChart) {
+      var sel = WS.selectionSave();
+      $('#insertChartModal .Chart').appendTo(sel.endContainer);
+      insertChart = false;
+    }
+  });
 
   // Insert Chart
-  $('#insert_chart').click(function(e) {
-    clearModal();
-    updateModal();
+  $('#insert_chart').click(function() {
+    exports.clearModal();
+    exports.updateModal();
     $('#insertChartModal').modal('show');
   });
   WS.registerDOMException('.Chart', function(obj) {
     return {
-      options: $(obj).data('chartOptions'),
-      style: $(obj).attr('style'),
-      class: $(obj).attr('class')
+      'options': $(obj).data('chartOptions'),
+      'style': $(obj).attr('style'),
+      'class': $(obj).attr('class')
     };
   }, function(json) {
     var id = 'tmp' + (Math.random() * 10000000 | 0);
     setTimeout(function() {
-      var chart = makeChart(null, json.options, {
+      var chart = exports.makeChart(null, json.options, {
         width: 558,
         height: 300
       });
       $('#' + id).replaceWith(chart[0]);
       chart[0].attr('style', json.style);
       chart[0].resize();
-      chart[0].attr('class', json.class);
+      chart[0].attr('class', json['class']);
     }, 1);
     return '<div id="' + id + '"></div>';
   });
-
-  // Create a palette of colors. Opacities can be undefined or in the format of [1, 0.5, 0].
-  self.colorWheel = function(number, opacities) {
-    var colors = [];
-
-    function makeHSLA(h, s, l, a) {
-      return 'hsla(' +
-        (h | 0) + ',' +
-        (s | 0) + '%,' +
-        (l | 0) + '%,' +
-        a + ')';
-    };
-    _.times(number, function(i) {
-      var h = i / number * 360;
-      var s = 100;
-      var l = 60;
-      if (opacities) {
-        var color = _.map(opacities, function(opacity) {
-          return makeHSLA(h, s, l, opacity);
-        });
-      } else {
-        var color = makeHSLA(h, s, l, 1);
-      }
-      colors.push(color);
-    });
-    return colors;
-  };
-
-  // Code to disable the function.
-  self.disable = function() {
-    $('*').unbind('.Chart');
-    $('*').undelegate('.Chart');
-    $('.Chart').remove();
-    WS.unregisterDOMException('.Chart');
-  };
-  // Return self so other modules can hook into this one.
-  return self;
+  // Return exports so other modules can hook into this one.
+  return exports;
 });

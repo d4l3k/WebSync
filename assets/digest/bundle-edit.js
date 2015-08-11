271650a8288e9f58c292b07b1379ad8f78a5a9e7
Tristan Rice <rice@fn.lc>
2015-08-11 13:18:32 -0700

    Updated and fixed dependencies

--- assets/digest/bundle-edit.js
+++ assets/digest/bundle-edit.js
@@ -3,7 +3,7 @@
 //= require bootstrap-switch/dist/js/bootstrap-switch.min.js
 //= require seiyria-bootstrap-slider/dist/bootstrap-slider.min.js
 //= require rangy/rangy-core.js
-//= require rangy/rangy-cssclassapplier.js
+//= require rangy/rangy-classapplier.js
 //= require lodash/lodash.min.js
 //= require jquery-transform/jquery.transform2d.js
 //= require fast-json-patch/dist/json-patch-duplex.min.js
